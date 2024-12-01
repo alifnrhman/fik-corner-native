6fd95fb3c0a66dc19708e4e799cf66d399cfafe1
Tsaqif Rakha <akurakha45@gmail.com>
2024-12-01 21:39:04 +0700

    Mau coba masukin dark mode

--- js/scripts.js
+++ js/scripts.js
@@ -55,3 +55,31 @@ document.addEventListener("DOMContentLoaded", () => {
       }
    });
 });
+
+//Dark mode
+let darkMode = localStorage.getItem('dark-mode');
+
+const enableDarkMode = () =>{
+   toggleBtn.classList.replace('fa-sun', 'fa-moon');
+   body.classList.add('dark');
+   localStorage.setItem('dark-mode', 'enabled');
+}
+
+const disableDarkMode = () =>{
+   toggleBtn.classList.replace('fa-moon', 'fa-sun');
+   body.classList.remove('dark');
+   localStorage.setItem('dark-mode', 'disabled');
+}
+
+if(darkMode === 'enabled'){
+   enableDarkMode();
+}
+
+toggleBtn.onclick = (e) =>{
+   darkMode = localStorage.getItem('dark-mode');
+   if(darkMode === 'disabled'){
+      enableDarkMode();
+   }else{
+      disableDarkMode();
+   }
+}
